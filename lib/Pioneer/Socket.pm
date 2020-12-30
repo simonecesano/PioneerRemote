0b73763be22a67f4e4554c17617231dc2a180d70
Simone Cesano <simonecesano@libero.it>
2020-12-30 19:47:48 +0100

    Added checks and improved recurring loop

--- lib/Pioneer/Socket.pm
+++ lib/Pioneer/Socket.pm
@@ -27,7 +27,7 @@ has 'frequency' => 1;
 
 has 'app';
 
-my $queue = Mojo::Collection->new;
+my $frequency = 1.5;
 my $app;
 
 around 'new' => sub {
@@ -43,12 +43,36 @@ around 'new' => sub {
 
     # $io->setsockopt(SOL_SOCKET, SO_RCVTIMEO, pack('l!l!', $ret->timeout, 0));
 
-    $app = $ret->app;
+    # $app = $ret->app;
 
     $ret->io($io);
 
     return $ret;
 };
+
+
+my $queue = Mojo::Collection->new;
+my $free = 1;
+Mojo::IOLoop->recurring(1 =>
+			sub {
+			    # print 'here', $free, $queue->size;
+			    if ($free && @{$queue}) {
+				my ($io, $p, $cmd, $e) = @{shift @{$queue}};
+				$free = 0;
+				my $res = 
+				    ('CODE' eq ref $cmd) ?
+				    $cmd->() :
+				    $io->cmd($cmd);
+				$io->cmd();
+				$p->resolve($res);
+				$free = 1;
+			    } else {
+				# print STDERR time;
+				if (@{$queue}) {
+				}
+			    }
+			});
+
 
 sub send { shift->io->send(@_) }
 
@@ -64,6 +88,7 @@ sub read {
     $self->recv($buffer, $length);
     return $buffer;
 }
+
 
 sub stream_done {
     my @lines = @_;
@@ -92,7 +117,7 @@ sub done {
     local $_ = $i;
 
     return 1 if  $_ =~ /^$/;
-    return 1 if  $_ =~ /^R\s*/;
+    return 1 if  $_ =~ /^R/;
 
     if ($_ =~ /^FN\d\d\s*/) { return 1 }
 
@@ -102,13 +127,16 @@ sub done {
 
     my $d = 0 + $1;
     # print $l, $1, $d, $c;
-    print STDERR dumper [ $i, $_ , $c, $d >= $c ];
+    # print STDERR dumper [ $i, $_ , $c, $d >= $c ];
     $d >= $c;
 }
 
 sub cmd {
     my $self = shift;
     my $cmd  = shift || '';
+    my $override = shift;
+
+    return [] unless $override || $self->allowed($cmd);
 
     my $sent = $self->send("$cmd\n");
 
@@ -128,6 +156,8 @@ sub cmd {
     # $self->send("\n");
     # print STDERR 'check', $self->read();
     # $self->io;
+    if (($lines[0] eq 'R') && ((scalar @lines) > 1)) { shift @lines }
+
     return [ map { decode('UTF-8', decode('UTF-8', $_) ); } @lines ];
 }
 
@@ -136,17 +166,13 @@ sub log {
     if ($self->app) {
 	$self->app->log->info(@_)
     } else {
-	print @_ . "\n";
+	print @_
     }
 }
-
-
 
 sub enqueue {
     my $self = shift;
     my $cmd  = shift;
-
-    print STDERR "in enqueue $cmd";
 
     # return $val;
     my $p = Mojo::Promise->new;
@@ -154,7 +180,7 @@ sub enqueue {
 
     push @{$queue}, [ $self, $p, $cmd, $t ];
 
-    print $queue->size;
+    # print $queue->size;
 
     return $p;
 }
@@ -163,41 +189,126 @@ sub cmd_p {
     my $self = shift;
     my $cmd = shift;
     my $prio = shift;
-    print STDERR "in cmd_p $cmd";
     return $self->enqueue($cmd, $prio)
 }
 
 sub get_screen_p {
     my $self = shift;
-    return $self->cmd_p('?GAP')
+    my $p = Mojo::Promise->new;
+    Mojo::Promise->all(
+		       $self->cmd_p('?F'),
+		       $self->cmd_p('?GAP')
+		      )
+	  ->then(sub {
+		     my ($source, $screen) = @_;
+		     $source = $source->[0][0];
+
+		     if ($source eq 'FN19' && $screen->[0][-1] =~ /GEP07023/) {
+			 $p->resolve($screen->[0]);
+		     } elsif ($source eq 'FN19' && $screen->[0][-1] !~ /GEP07023/) {
+			 $p->resolve([ 'GBP00' ] );
+		     } elsif ($source eq 'FN10') {
+			 $p->resolve([ 'GBP00' ] );
+		     } else {
+			 $p->resolve($screen->[0]);
+		     }
+		 });
+	  return $p;
 }
 
+sub _fix_source {
+    my $res = shift;
+    return join '', reverse unpack 'a2a2', $res;
+};
 
-# sub start {
-#     print Mojo::IOLoop->is_running;
+sub source {
+    my $self = shift;
+    my $source = shift;
 
-my $free = 1;
-my $frequency = 1.5;
+    my $current_source = $self->cmd('?F');
 
-Mojo::IOLoop->recurring(1 =>
-			sub {
-			    # print 'here', $free, $queue->size;
-			    if ($free && @{$queue}) {
-				my ($io, $p, $cmd, $e) = @{shift @{$queue}};
-				$free = 0;
-				my $res = $io->cmd($cmd);
-				$io->cmd();
-				$p->resolve($res);
-				# print STDERR dumper [ $cmd, $res ];
-				sleep 0.3;
-				$free = 1;
-			    } else {
-				if (@{$queue}) {
-				}
-			    }
-			});
-#     Mojo::IOLoop->singleton->start unless Mojo::IOLoop->singleton->is_running;
-# }
+    return $current_source unless $source;
 
-# Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+    if ($current_source->[0] ne _fix_source($source)) {
+	return $self->cmd($source);
+    } else {
+	return $current_source;
+    }
+}
+
+sub source_p {
+    my $self = shift;
+    my $source = shift;
+
+    return $self->enqueue(sub {
+			      $self->source($source)
+			  });
+}
+
+# ----------------------------------------------------------------------------------
+# allowed checks here
+# ----------------------------------------------------------------------------------
+
+my $checks = {
+	      'generic' => sub {
+		  my $self = shift;
+		  my $cmd  = shift;
+		  # do something
+		  # check result
+		  # allow or reject
+		  return 1;
+	      },
+	      '10FN' => sub {
+		  my $self = shift;
+		  my $cmd  = shift;
+		  # do something
+		  my $r = $self->cmd('?F');
+		  return $r->[0] ne 'FN10' && $r->[0] ne 'FN19';
+		  # check result
+		  # allow or reject
+	      },
+	      '13FN' => sub {
+		  my $self = shift;
+		  my $cmd  = shift;
+		  print 'here';
+
+		  my $r = $self->cmd('?F');
+		  return $r->[0] ne 'FN13';
+	      },
+	      '31PB' => sub {
+		  my $self = shift;
+		  my $cmd  = shift;
+
+		  my $r = $self->cmd('?GAP');
+		  return grep { /GCP0\d{5,5}10/ } @$r;
+	      },
+	      # play
+	      '10PB' => sub { return shift->cmd('?F')->[0] eq 'FN19' },
+	      # pause
+	      '11PB' => sub { return shift->cmd('?F')->[0] eq 'FN19' },
+	      # skip reverse
+	      '12PB' => sub { return shift->cmd('?F')->[0] eq 'FN19' },
+	      # skip forward
+	      '13PB' => sub { return shift->cmd('?F')->[0] eq 'FN19' },
+	      # stop
+	      '20PB' => sub { return shift->cmd('?F')->[0] eq 'FN19' },
+};
+
+# Skip Reverse Key	12PB
+# Stop Key	20PB
+# Skip Forward Key	13PB
+# Pause Key	11PB
+# Play Key	10PB
+
+sub allowed {
+    my $self = shift;
+    my $cmd = shift;
+
+    return 1 unless $cmd;
+
+    return 1 unless $checks->{$cmd};
+
+    return $checks->{$cmd}->($self, $cmd);
+}
+
 1;
